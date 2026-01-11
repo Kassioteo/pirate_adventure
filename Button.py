b3a66bad0e2cbff85dc2f82880a1467457e5d8a2
Kassioteo <kassioteadoro@gmail.com>
2026-01-11 05:10:36 -0300

    refatorando sprites

--- Button.py
+++ Button.py
@@ -7,7 +7,7 @@ class Button:
 
 
 play_button = Button("play_button", (450, 400))
-exit_butoon = Button("exit_button", (940, 20))
-music_button = Button("exit_button", (40, 20))
-exit_game_button = Button("play_button", (450, 450))
-return_button = Button("play_button", (450, 350))
+exit_butoon = Button("exit_button", (910, 40))
+music_button = Button("music_button", (40, 35))
+exit_game_button = Button("exit_game_button", (450, 450))
+return_button = Button("return_button", (450, 350))
